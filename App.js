2fe8244c88fd2b7659d958dc2cfe9b75591d1400
pgonzalesc <pedro281195@gmail.com>
2020-12-12 11:08:08 -0500

    Inicializacion Hackaton 10

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, Buttom } from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
